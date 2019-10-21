694025cf3c7907f2b30f96fe021c5c12f7840337
Jens Ameskamp <jens@ameskamp.de>
2019-10-21 19:55:27 +0200

    Token creation

--- insert_token.py
+++ insert_token.py
@@ -1,8 +1,28 @@
 from redis import Redis
 import pickle
 from download_token import DownloadToken
+import string
+import random
+import sys
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 redis = Redis(host='localhost', port=6379, db=4)
-token = DownloadToken("ASD", [{'path': 'app.py', 'description': 'Main File' }, { 'path':'.gitignore', 'description': 'Another file'}])
+
+tokenId = id_generator(4, "23456789") + "-" + id_generator(4, "23456789")
+
+files = []
+
+i = 1
+while i < len(sys.argv):
+    path = sys.argv[i]
+    description = sys.argv[i + 1]
+    file = {'path': path , 'description': description }
+    files.append(file)
+    i += 2
+
+token = DownloadToken(tokenId, files)
+print(f"Created token with ID {tokenId}. Contents: {str(files)}")
 
 redis.set(token.id, pickle.dumps(token))
